0c974c9ccf38da848cbba9374b4e1381ac6734da
shuremali02 <shuremsyed41@gmail.com>
2025-01-28 04:25:49 -0800

    header issue fixed

--- app/Component/header.tsx
+++ app/Component/header.tsx
@@ -15,8 +15,8 @@ const BlogHeader = () => {
   return (
     <header className="bg-gradient-to-r from-purple-500 via-pink-500 to-red-500  sticky top-0 z-50">
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-        <h1 className="text-3xl font-bold font-sans text-gray-500">My Blog</h1>
-        <nav className="md:flex space-x-6 text-lg font-semibold " >
+       <Link href={"/"}> <h1 className="text-3xl font-bold font-sans text-black hover:text-gray-200">My Blog</h1></Link>
+        <nav className="md:flex hidden space-x-6 text-lg font-semibold " >
           <Link className="hover:text-white" href="#">Home</Link>
           <Link className="hover:text-white" href="/blog" >Blog</Link>
           <Link className="hover:text-white " href="#" >About</Link>
@@ -29,11 +29,11 @@ const BlogHeader = () => {
         </Button>
       </div>
       {menuOpen && (
-        <div className="md:hidden bg-white shadow-md">
+        <div className="md:hidden bg-gradient-to-r from-blue-500 via-purple-600 to-blue-500 text-white shadow-md flex items-center justify-center h-screen">
           <nav className="flex flex-col space-y-4 px-4 py-4">
-          <Link className="hover:text-white" href="#">Home</Link>
+          <Link className="hover:text-white" href="/">Home</Link>
           <Link className="hover:text-white" href="/blog" >Blog</Link>
-          <Link className="hover:text-white " href="#" >About</Link>
+          <Link className="hover:text-white " href="/" >About</Link>
           </nav>
         </div>
       )}
